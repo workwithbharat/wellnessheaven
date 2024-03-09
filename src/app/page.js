b7d2b46169766f37a82171b2c2535890389465ef
Bharat Kumar <workwithbharat@outlook.com>
2024-03-09 20:39:38 +0530

    Update page.js
    done

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <nav className="navbar px-2 py-2 is-link is-size-5 is-justify-content-center is-align-content-center is-flex-wrap-wrap" role="navigation" aria-label="main navigation">
-        <p className="is-size-4 is-size-5-touch has-text-centered">96% of patient's <span className="yellowunder">dropped 29.4 lbs</span> of Fat with Dr's rare method</p>
+        <p className="is-size-4 is-size-5-touch has-text-centered">96% of women's <span className="yellowunder">dropped 29.4 lbs</span> of Fat with Dr's rare method</p>
       </nav>
       <section className="section is-paddingless">
         <div className="container container-limit px-2 py-6">
@@ -10,12 +10,12 @@ export default function Home() {
             <div className="column index-column index-column-2 mr-5 is-hidden-desktop"></div>
             <div className="column is-8-desktop is-9-touch">
               <p className="has-text-weight-bold has-text-left is-size-3 is-size-4-touch lh1 mb-3 pt-3" style={{ color: '#098974' }}>This is going to surprise you...</p>
-              <p className="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-5">A little-known whistleblowing doctor walked into a <span className="yellowunder">top US hospital</span> with this common pantry food, prepared it and gave it to the <span className="yellowunder">overweight patients</span>...</p>
+              <p className="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-5">A little-known whistleblowing doctor walked into a <span className="yellowunder">top US hospital</span> with this common pantry food, prepared it and gave it to the <span className="yellowunder">overweight womens</span>...</p>
               <p className="is-size-4 mb-3">And within weeks 96% of them had dropped an <span className="yellowunder">average of 29.4 lbs</span>, supporting healthy blood sugar and blood pressure...</p>
-              <p className="is-size-4 mb-3">How the doctor's <strong><a href="https://hop.clickbank.net/?affiliate=aslibharat&vendor=puraclick&pg=research">unusual method</a></strong> works is nothing short of incredible... and it has changed everything we thought we knew about weight loss.</p>
+              <p className="is-size-4 mb-3">How the doctor's <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">unusual method</a></strong> works is nothing short of incredible... and it has changed everything we thought we knew about weight loss.</p>
               <p className="is-size-4 mb-3">The craziest part? You can <span className="yellowunder">do this method for yourself</span> in less than 30 seconds a day... With one simple pantry food.</p>
-              <p className="notification is-size-4" style={{ border: '2px solid #136e73', font: '20px' }}>If you have weight concerns, you should try this method for yourself right now. The $80 billion dollar weight loss industry do not want you to see <strong><a href="https://hop.clickbank.net/?affiliate=aslibharat&vendor=puraclick&pg=research">THIS VIDEO</a></strong> and discover the amazing fat-melting pantry food. Watch it now before it's too late.</p>
-              <p className="is-size-4">Skeptical? I was too, but then I saw <strong><a href="https://hop.clickbank.net/?affiliate=aslibharat&vendor=puraclick&pg=research">this shocking proof</a></strong> for myself.</p>
+              <p className="notification is-size-4" style={{ border: '2px solid #136e73', font: '20px' }}>If you have weight concerns, you should try this method for yourself right now. The $80 billion dollar weight loss industry do not want you to see <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">THIS VIDEO</a></strong> and discover the amazing fat-melting pantry food. Watch it now before it's too late.</p>
+              <p className="is-size-4">Skeptical? I was too, but then I saw <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">this shocking proof</a></strong> for myself.</p>
               <div className="buttons mt-5"><a href="https://60897y7kp5sxxw3f8hh6weg77u.hop.clickbank.net" className="button is-link is-size-5 is-size-6-touch topbar" rel="order"><strong>Try it for yourself now!</strong></a></div>
             </div>
             <div className="column index-column index-column-2 ml-5 is-hidden-touch"></div>
@@ -26,7 +26,7 @@ export default function Home() {
         <div className="container container-limit px-2 py-6 flex">
         <p className="has-text-weight-bold has-text-centered is-size-3 is-size-4-touch lh1 mb-5 pt-3" style={{ color: '#098974' }}><span className="yellowunder">Watch this shocking proof</span>...</p>
         <div className="is-flex is-justify-content-center is-align-content-center">
-          <a href="https://hop.clickbank.net/?affiliate=aslibharat&vendor=puraclick&pg=research">
+          <a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">
           <img src="https://i.ibb.co/df0Mr2N/video-image.png" />
           </a>
         </div>

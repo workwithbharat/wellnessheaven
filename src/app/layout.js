2e00c73e194b778aa8aa0f39bfd8436c9143e4ba
Bharat Kumar <workwithbharat@outlook.com>
2024-03-09 16:18:35 +0530

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,18 @@ export default function RootLayout({ children }) {
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','G-G0C0CYBJDE');`}} />
     <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/gtag/js?id=G-G0C0CYBJDE" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+    <script dangerouslySetInnerHTML={{__html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '939966840631518');
+fbq('track', 'PageView');`}} />
+<noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.facebook.com/tr?id=939966840631518&ev=PageView&noscript=1" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+
       </body>
     </html>
   );

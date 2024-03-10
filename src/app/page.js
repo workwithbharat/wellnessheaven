e0c707826d286fba929e5ad14fb5675853997021
Bharat Kumar <workwithbharat@outlook.com>
2024-03-10 08:57:13 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,12 @@ export default function Home() {
             <div className="column is-8-desktop is-9-touch hellobharat-width">
               <img src="https://media.istockphoto.com/id/629771548/photo/surprised-happy-beautiful-woman-looking-sideways-in-excitement-isolated.webp?b=1&s=612x612&w=0&k=20&c=5E8ZpzYuLkyJithSPTs4nBxQCblL4aL4LvkFngUkLEg=" className="hellobharat-only-mobile mb-2"></img>
               <p className="has-text-weight-bold has-text-left is-size-3 is-size-4-touch lh1 mb-3 pt-3" style={{ color: '#098974' }}>This is going to surprise you...</p>
-              <p className="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-5">A little-known whistleblowing doctor walked into a <span className="yellowunder">top US hospital</span> with this common pantry food, prepared it and gave it to the <span className="yellowunder">overweight womens</span>...</p>
+              <p className="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-5">A little-known whistleblowing doctor walked into a <span className="yellowunder">top US hospital</span> with this Fast-Acting Formula, prepared it and gave it to the <span className="yellowunder">overweight womens</span>...</p>
               <p className="is-size-4 mb-3">And within weeks 96% of them had dropped an <span className="yellowunder">average of 29.4 lbs</span>, supporting healthy blood sugar and blood pressure...</p>
-              <p className="is-size-4 mb-3">How the doctor's <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">unusual method</a></strong> works is nothing short of incredible... and it has changed everything we thought we knew about weight loss.</p>
-              <p className="is-size-4 mb-3">The craziest part? You can <span className="yellowunder">do this method for yourself</span> in less than 30 seconds a day... With one simple pantry food.</p>
-              <p className="notification is-size-4" style={{ border: '2px solid #136e73', font: '20px' }}>If you have weight concerns, you should try this method for yourself right now. The $80 billion dollar weight loss industry do not want you to see <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">THIS VIDEO</a></strong> and discover the amazing fat-melting pantry food. Watch it now before it's too late.</p>
-              <p className="is-size-4">Skeptical? I was too, but then I saw <strong><a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">this shocking proof</a></strong> for myself.</p>
+              <p className="is-size-4 mb-3">How the doctor's <strong><a href="https://9337461dwys0s768jh97ezcl3p.hop.clickbank.net">unusual method</a></strong> works is nothing short of incredible... and it has changed everything we thought we knew about weight loss.</p>
+              <p className="is-size-4 mb-3">The craziest part? You can <span className="yellowunder">do this method for yourself</span> in less than 30 seconds a day... With one simple juice.</p>
+              <p className="notification is-size-4" style={{ border: '2px solid #136e73', font: '20px' }}>If you have weight concerns, you should try this method for yourself right now. The $80 billion dollar weight loss industry do not want you to see <strong><a href="https://9337461dwys0s768jh97ezcl3p.hop.clickbank.net">THIS VIDEO</a></strong> and discover the amazing fat-melting special juice. Watch it now before it's too late.</p>
+              <p className="is-size-4">Skeptical? I was too, but then I saw <strong><a href="https://9337461dwys0s768jh97ezcl3p.hop.clickbank.net">this shocking proof</a></strong> for myself.</p>
               <p className="is-size-4">This has garnered the trust of 127,000+ womens, each over the age of 50+</p>
               <div className="buttons mt-5"><a href="https://60897y7kp5sxxw3f8hh6weg77u.hop.clickbank.net" className="hellobharat-button button is-link is-size-5 is-size-6-touch topbar" rel="order"><strong>Try it for yourself now!</strong></a></div>
             </div>
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="container container-limit px-2 py-6 flex">
         <p className="has-text-weight-bold has-text-centered is-size-3 is-size-4-touch lh1 mb-5 pt-3" style={{ color: '#098974' }}><span className="yellowunder">Watch this shocking proof</span>...</p>
         <div className="is-flex is-justify-content-center is-align-content-center">
-          <a href="https://59b3c219p5u2382m6ivhwkk86b.hop.clickbank.net">
+          <a href="https://9337461dwys0s768jh97ezcl3p.hop.clickbank.net">
           <img src="https://i.ibb.co/df0Mr2N/video-image.png" />
           </a>
         </div>
